56467f9132be91352decb868f2a88884bec31717
seojh5939 <76927858+seojh5939@users.noreply.github.com>
2023-07-03 17:13:16 +0900

    상단 타이틀 이미지 & 텍스트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        body: Column(
+          children: [
+            titleImageText("버킷리스트"),
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget titleImageText(String title) {
+  return Stack(
+    alignment: Alignment.center,
+    children: [
+      // Image
+      Image.asset('assets/images/banner_Img.png'),
+      // Title
+      Center(
+          child: Text(
+        title,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 48,
+        ),
+      )),
+    ],
+  );
 }
