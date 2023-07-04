51c26824809058c868cfab8b0ec276ed6bb879a7
추민수 <devsoftychoo@gmail.com>
2023-07-04 21:41:30 +0900

    [DESIGN] : 🎨
    Home(main) UI 완성

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
               children: [
                 //titleImageText("버킷리스트 \u{1F60D}"),
                 SizedBox(
-                  height: 10,
+                  height: 30,
                 ),
                 Row(
                   children: [
@@ -82,163 +82,166 @@ class MyApp extends StatelessWidget {
                   height: 30,
                 ),
                 Expanded(
-                  child: Container(
-                    color: const Color.fromARGB(255, 107, 189, 255),
-                    child: ListView.builder(
-                      itemCount: bucketList.length,
-                      itemBuilder: (context, index) {
-                        final item = bucketList[index];
-                        return Padding(
-                          padding: const EdgeInsets.all(3.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
+                  child: ListView.builder(
+                    itemCount: bucketList.length,
+                    itemBuilder: (context, index) {
+                      final item = bucketList[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ListTile(
+                            title: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    bucketListItemService.changeCheckBox(index);
+                                  },
+                                  icon: Icon(
+                                    bucketListItemService
+                                            .bucketList[index].isCompleted
+                                        ? Icons.check_box
+                                        : Icons.check_box_outline_blank_rounded,
+                                  ),
+                                  color: Colors.green,
+                                ),
+                                Text(item.content),
+                              ],
                             ),
-                            child: ListTile(
-                              title: Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      bucketListItemService
-                                          .changeCheckBox(index);
-                                    },
-                                    icon: Icon(
-                                      bucketListItemService
-                                              .bucketList[index].isCompleted
-                                          ? Icons.check_box
-                                          : Icons
-                                              .check_box_outline_blank_rounded,
-                                    ),
-                                    color: Colors.green,
-                                  ),
-                                  Text(item.content),
-                                ],
-                              ),
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  IconButton(
-                                    icon: Icon(Icons.edit),
-                                    onPressed: () {
-                                      showDialog(
-                                        context: context,
-                                        builder: (context) {
-                                          TextEditingController controller =
-                                              TextEditingController(
-                                                  text: item.content);
-                                          return AlertDialog(
-                                            title: Text('수정하기'),
-                                            content: TextField(
-                                              controller: controller,
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.edit),
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        TextEditingController controller =
+                                            TextEditingController(
+                                                text: item.content);
+                                        return AlertDialog(
+                                          title: Text('수정하기'),
+                                          content: TextField(
+                                            controller: controller,
+                                          ),
+                                          actions: [
+                                            ElevatedButton(
+                                              onPressed: () {
+                                                bucketListItemService
+                                                    .updateItem(
+                                                        index: index,
+                                                        content:
+                                                            controller.text);
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text('저장'),
                                             ),
-                                            actions: [
-                                              ElevatedButton(
-                                                onPressed: () {
-                                                  bucketListItemService
-                                                      .updateItem(
-                                                          index: index,
-                                                          content:
-                                                              controller.text);
-                                                  Navigator.pop(context);
-                                                },
-                                                child: Text('저장'),
-                                              ),
-                                              ElevatedButton(
-                                                onPressed: () {
-                                                  Navigator.pop(context);
-                                                },
-                                                child: Text('취소'),
-                                              ),
-                                            ],
-                                          );
-                                        },
-                                      );
-                                    },
-                                  ),
-                                  IconButton(
-                                    icon: Icon(Icons.delete),
-                                    onPressed: () {
-                                      showDialog(
-                                        context: context,
-                                        builder: (context) {
-                                          return AlertDialog(
-                                            title: Text('삭제하기'),
-                                            content: Text('지우시겠습니까?'),
-                                            actions: [
-                                              ElevatedButton(
-                                                onPressed: () {
-                                                  bucketListItemService
-                                                      .removeItem(index: index);
-                                                  Navigator.pop(context);
-                                                },
-                                                child: Text('삭제하기'),
-                                              ),
-                                              ElevatedButton(
-                                                onPressed: () {
-                                                  Navigator.pop(context);
-                                                },
-                                                child: Text('취소하기'),
-                                              ),
-                                            ],
-                                          );
-                                        },
-                                      );
-                                    },
-                                  ),
-                                ],
-                              ),
+                                            ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text('취소'),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return AlertDialog(
+                                          title: Text('삭제하기'),
+                                          content: Text('지우시겠습니까?'),
+                                          actions: [
+                                            ElevatedButton(
+                                              onPressed: () {
+                                                bucketListItemService
+                                                    .removeItem(index: index);
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text('삭제하기'),
+                                            ),
+                                            ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text('취소하기'),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  },
+                                ),
+                              ],
                             ),
                           ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => BucketDoneList()),
+                        ),
                       );
                     },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          Color.fromARGB(255, 245, 122, 74)),
-                    ),
-                    child: Text(
-                      '버킷리스트 등록!',
-                      style: TextStyle(
-                        fontSize: 20,
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BucketDoneList()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              50), // 반지름 값을 조정하여 동그란 모양으로 설정
+                        ),
+                        backgroundColor:
+                            Color.fromARGB(255, 245, 122, 74), // 주황색으로 설정
+                      ),
+                      child: Text(
+                        '버킷리스트 등록!',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
+                    // child: Row(
+                    //   children: [
+                    //     Expanded(
+                    //       child: TextField(
+                    //         controller: textEditingController,
+                    //         decoration: InputDecoration(
+                    //           hintText: '이루고 싶은 것을 입력하세요',
+                    //         ),
+                    //       ),
+                    //     ),
+                    //     ElevatedButton(
+                    //       onPressed: () {
+                    //         bucketListItemService.addItem(
+                    //           content: textEditingController.text,
+                    //         );
+                    //         // 기기에 저장
+                    //         bucketListItemService.saveBucketItem();
+                    //         textEditingController.clear();
+                    //       },
+                    //       child: Text('기록하기'),
+                    //     ),
+                    //   ],
+                    // ),
                   ),
-                  // child: Row(
-                  //   children: [
-                  //     Expanded(
-                  //       child: TextField(
-                  //         controller: textEditingController,
-                  //         decoration: InputDecoration(
-                  //           hintText: '이루고 싶은 것을 입력하세요',
-                  //         ),
-                  //       ),
-                  //     ),
-                  //     ElevatedButton(
-                  //       onPressed: () {
-                  //         bucketListItemService.addItem(
-                  //           content: textEditingController.text,
-                  //         );
-                  //         // 기기에 저장
-                  //         bucketListItemService.saveBucketItem();
-                  //         textEditingController.clear();
-                  //       },
-                  //       child: Text('기록하기'),
-                  //     ),
-                  //   ],
-                  // ),
                 ),
                 SizedBox(
                   height: 50,
