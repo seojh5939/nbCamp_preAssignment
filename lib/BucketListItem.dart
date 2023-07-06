c3a0d74bca3fc14c076f37f4be18b3e1dfd33963
SeoJungHan <seojh5939@gmail.com>
2023-07-06 15:35:40 +0900

    [FIX] : 🔧 생성시 필요한 요소 추가(제목, 날짜)

--- lib/BucketListItem.dart
+++ lib/BucketListItem.dart
@@ -2,21 +2,29 @@
 class BucketListItem {
   BucketListItem({
     this.isCompleted = false,
+    this.title = "",
     required this.content,
+    this.dttm = "",
   });
-
+  String title;
   String content;
+  String dttm;
   bool isCompleted;
 
   Map toJson() {
     return {
       'isCompleted': isCompleted,
+      'title': title,
       'content': content,
+      'dttm': dttm,
     };
   }
 
   factory BucketListItem.fromJson(json) {
     return BucketListItem(
-        isCompleted: json['isCompleted'], content: json['content']);
+        isCompleted: json['isCompleted'],
+        title: json['title'],
+        content: json['content'],
+        dttm: json['dttm']);
   }
 }
