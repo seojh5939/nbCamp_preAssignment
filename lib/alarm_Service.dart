4d2873efb188fc90dcb160030f90331a32c8a4e1
SeoJungHan <seojh5939@gmail.com>
2023-07-06 19:31:47 +0900

    [FIX] : 🔧 removeAlarmItem에 notifyListener 추가 list remove -> removeAt으로 변

--- lib/alarm_Service.dart
+++ lib/alarm_Service.dart
@@ -1,4 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/alarm.dart';
 
 // 알림 Data 관리
-class AlarmService extends ChangeNotifier {}
+class AlarmService extends ChangeNotifier {
+  List<Alarm> alarmList = [];
+
+  addAlarmItem({
+    required String year,
+    required String month,
+    required String day,
+    required String hour,
+    required String min,
+  }) {
+    alarmList
+        .add(Alarm(year: year, month: month, day: day, hour: hour, min: min));
+    notifyListeners();
+  }
+
+  updateAlarmItem({
+    required int index,
+    required String year,
+    required String month,
+    required String day,
+    required String hour,
+    required String min,
+  }) {
+    alarmList[index].year = year;
+    alarmList[index].month = month;
+    alarmList[index].day = day;
+    alarmList[index].hour = hour;
+    alarmList[index].min = min;
+    notifyListeners();
+  }
+
+  removeAlarmItem({required index}) {
+    alarmList.remove(index);
+    notifyListeners();
+  }
+}
