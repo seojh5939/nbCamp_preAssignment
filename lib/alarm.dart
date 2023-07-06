4aabb33139f9b7a6a94caf2dc48bd0b62e4ece10
SeoJungHan <seojh5939@gmail.com>
2023-07-06 19:26:22 +0900

    [FEAT] : ⚙️ 알람 model 및 service 구

---
+++ lib/alarm.dart
@@ -0,0 +1,15 @@
+/// 알림 데이터
+class Alarm {
+  Alarm({
+    required this.year,
+    required this.month,
+    required this.day,
+    required this.hour,
+    required this.min,
+  });
+  String year;
+  String month;
+  String day;
+  String hour;
+  String min;
+}
---
+++ lib/alarm_Service.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+// 알림 Data 관리
+class AlarmService extends ChangeNotifier {}
